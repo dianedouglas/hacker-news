969ed0fa0460e769db30707aa8d4d3258f1beefd
Diane Douglas & moof mayeda <student@epicodus.com>
2014-09-05 10:50:32 -0700

    allow users to update links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -20,6 +20,19 @@ class LinksController < ApplicationController
     @link = Link.find(params[:id])
   end
 
+  def edit
+    @link = Link.find(params[:id])
+  end
+
+  def update
+    @link = Link.find(params[:id])
+    if @link.update(link_params)
+      redirect_to link_path(@link), notice: "Link updated! Congrats!"
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
     @link = Link.find(params[:id])
     @link.destroy
