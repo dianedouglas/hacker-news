91c849ccdb6265619476d14efcf143ed57c0a1ef
Diane Douglas & moof mayeda <student@epicodus.com>
2014-09-05 10:26:13 -0700

    Add form and errors partial for creating new links.

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,23 @@
 class LinksController < ApplicationController
+  def index
+    @links = Link.all
+  end
+
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+      redirect_to root_url, notice: "New Link submitted! Congrats!"
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def link_params
+    params.require(:link).permit(:name, :url, :description)
+  end
 end
