80a003af68cda79ae5c68d9da493f95352b0a3bd
Diane Douglas & moof mayeda <student@epicodus.com>
2014-09-05 11:04:24 -0700

    allow users to comment on a link.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  def new
+    @link = Link.find(params[:link_id])
+    @comment = Comment.new
+  end
+
+  def create
+    @link = Link.find(params[:link_id])
+    @comment = @link.comments.new(comment_params)
+    if @comment.save
+      redirect_to link_path(@link), notice: "New comment submitted! Congrats!"
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:description)
+  end
 end
